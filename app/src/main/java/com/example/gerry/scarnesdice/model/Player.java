ec0cfa781703022018ba9be21e343336da17dacc
Gerry Fernando Patia <fernando_nxt@hotmail.com>
2017-04-13 23:22:21 -0700

    Player class updated

--- app/src/main/java/com/example/gerry/scarnesdice/model/Player.java
+++ app/src/main/java/com/example/gerry/scarnesdice/model/Player.java
@@ -5,4 +5,36 @@ package com.example.gerry.scarnesdice.model;
  */
 
 public class Player {
+
+    private int overallScore;
+    private int turnScore;
+
+    public Player(int overallScore, int turnScore) {
+        this.overallScore = overallScore;
+        this.turnScore = turnScore;
+    }
+
+    public int getOverallScore() {
+        return overallScore;
+    }
+
+    public void setOverallScore(int overallScore) {
+        this.overallScore = overallScore;
+    }
+
+    public int getTurnScore() {
+        return turnScore;
+    }
+
+    public void setTurnScore(int turnScore) {
+        this.turnScore = turnScore;
+    }
+
+    public void updateScoreWithDiceValue(int diceVal) {
+        if(diceVal != 1) {
+            turnScore += diceVal;
+        } else {
+            turnScore = 0;
+        }
+    }
 }
