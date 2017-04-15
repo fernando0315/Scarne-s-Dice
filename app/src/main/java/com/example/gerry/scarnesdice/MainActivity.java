789656be4b93050e62a382697b8c0edce746d90b
Gerry Fernando Patia <fernando_nxt@hotmail.com>
2017-04-14 17:35:31 -0700

    add timer for computer turn & roll dice function

--- app/src/main/java/com/example/gerry/scarnesdice/MainActivity.java
+++ app/src/main/java/com/example/gerry/scarnesdice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.gerry.scarnesdice;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,11 +13,11 @@ import com.example.gerry.scarnesdice.model.Player;
 
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private Player user;
     private Player computer;
-    private Random rand = new Random();
 
     private Button rollButton;
     private Button holdButton;
@@ -23,42 +25,39 @@ public class MainActivity extends AppCompatActivity {
     private ImageView diceImage;
     private TextView scoreTextView;
 
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            if(computerTurn() != 1 && computer.getTurnScore() < 20)
+                timerHandler.postDelayed(this, 1000);
+            else
+                cleanUp();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user = new Player(0,0);
-        computer = new Player(0,0);
+        user = new Player(0, 0);
+        computer = new Player(0, 0);
 
-        rollButton = (Button) findViewById(R.id.rollButton);
         diceImage = (ImageView) findViewById(R.id.diceImage);
         scoreTextView = (TextView) findViewById(R.id.scoreTextView);
+
+        rollButton = (Button) findViewById(R.id.rollButton);
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int diceValue = rand.nextInt(6) + 1;
-
-                switch(diceValue) {
-                    case 1: diceImage.setImageResource(R.drawable.dice1);
-                        break;
-                    case 2: diceImage.setImageResource(R.drawable.dice2);
-                        break;
-                    case 3: diceImage.setImageResource(R.drawable.dice3);
-                        break;
-                    case 4: diceImage.setImageResource(R.drawable.dice4);
-                        break;
-                    case 5: diceImage.setImageResource(R.drawable.dice5);
-                        break;
-                    default:diceImage.setImageResource(R.drawable.dice6);
-                        break;
-                }
-
+                int diceValue = rollDice();
                 user.updateTurnScoreWithDiceValue(diceValue);
-                printScore("user");
 
-                if(diceValue == 1)
-                    computerTurn();
+                printScore("user");
+                if (diceValue == 1)
+                    timerHandler.postDelayed(timerRunnable, 1000);
             }
         });
 
@@ -68,8 +67,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 user.updateOverallScore();
                 user.setTurnScore(0);
-                printScore("user");
-                computerTurn();
+                printScore("computer");
+
+                timerHandler.postDelayed(timerRunnable, 1000);
             }
         });
 
@@ -77,41 +77,69 @@ public class MainActivity extends AppCompatActivity {
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                timerHandler.removeCallbacks(timerRunnable);
                 computer.setOverallScore(0);
                 computer.setTurnScore(0);
                 user.setOverallScore(0);
                 user.setTurnScore(0);
-                printScore("user");
+                cleanUp();
             }
         });
-
     }
 
-    private void computerTurn() {
+    private int computerTurn() {
         rollButton.setEnabled(false);
         holdButton.setEnabled(false);
 
         int diceValue;
-        do{
-            diceValue = rand.nextInt(6) + 1;
+        //do{
+            diceValue = rollDice() ;
             computer.updateTurnScoreWithDiceValue(diceValue);
             printScore("computer");
-        } while(computer.getTurnScore() < 20 && diceValue != 1);
-
-        computer.updateOverallScore();
-        printScore("user");
-
-
-        rollButton.setEnabled(true);
-        holdButton.setEnabled(true);
+        //} while(computer.getTurnScore() < 20 && diceValue != 1);
+            return diceValue;
     }
 
     private void printScore(String turnId) {
-
         scoreTextView.setText("Your score:" + user.getOverallScore() +
                 " computer score: " + computer.getOverallScore() +
                 " " + turnId + " turn score: " +
                 (turnId.equals("user") ? user.getTurnScore() : computer.getTurnScore()));
     }
 
+    private void cleanUp() {
+        computer.updateOverallScore();
+        computer.setTurnScore(0);
+        printScore("user");
+        rollButton.setEnabled(true);
+        holdButton.setEnabled(true);
+    }
+
+    private int rollDice() {
+        Random rand = new Random();
+        int diceValue = rand.nextInt(6) + 1;
+
+        switch(diceValue) {
+            case 1:
+                diceImage.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                diceImage.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                diceImage.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                diceImage.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                diceImage.setImageResource(R.drawable.dice5);
+                break;
+            default:
+                diceImage.setImageResource(R.drawable.dice6);
+                break;
+        }
+
+        return diceValue;
+    }
 }
